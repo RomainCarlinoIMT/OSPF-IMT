8e74b83782c72f33bbfe99d2a8a126daf0b66e10
Romain Carlino <romain.carlino@etu.univ-grenoble-alpes.fr>
2025-06-06 14:29:17 +0200

    Possibilty to send formated messages

--- client.cpp
+++ client.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <cstring>
 
-int main()
+int send_message(const std::string& message)
 {
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     struct sockaddr_in server_address;
@@ -22,12 +22,21 @@ int main()
     server_address.sin_addr.s_addr = inet_addr("239.0.0.1"); // Multicast address
 
     // sending a message to the server
-    const char* message = "Hello, Server!";
-    ssize_t bytes_sent = sendto(socket_fd, message, strlen(message), 0,
+    ssize_t bytes_sent = sendto(socket_fd, message.c_str(), strlen(message.c_str()), 0,
                                 (struct sockaddr*)&server_address, sizeof(server_address));
     if (bytes_sent < 0) {
         perror("Failed to send message");
         close(socket_fd);
         return 1;
     }
+
+    return 0;
+}
+
+int main()
+{
+    std::string message = "Hello, UDP Server!";
+    send_message(message);
+    send_message("Another message to the server");
+    return 0;
 }
